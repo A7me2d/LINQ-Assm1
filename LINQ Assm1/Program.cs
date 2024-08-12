7d50ad6040503f7d00611159efe720fb8feaf729
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-08-12 14:40:43 +0300

    Add project files.

---
+++ LINQ Assm1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_Assm1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
